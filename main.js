8c6fc5d7665243ceee24da534afa342b83de12d8
ribbeiroana <anapribbeiro@gmail.com>
2024-04-12 00:33:47 -0300

    excluide 2 pages

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function calcularTotal() {
   var myCarousel = document.querySelector('#carouselExample');
   var carousel = new bootstrap.Carousel(myCarousel);
 
-  document.addEventListener('DOMContentLoaded', function() {
+  /*document.addEventListener('DOMContentLoaded', function() {
     const switcher = document.querySelector('btn1');
 
     switcher.addEventListener('click', function() {
@@ -28,4 +28,44 @@ function calcularTotal() {
 
         console.log('current class name: ' + className);
     });
-});
+});*/
+function initMap(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    var mapOptions = {
+      center: {lat: lat, lng: lng},
+      zoom: 12
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: {lat: lat, lng: lng},
+      map: map,
+      title: 'Você está aqui!'
+    });
+  }
+
+  function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        alert("Permissão para geolocalização negada pelo usuário.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Informações de localização estão indisponíveis.");
+        break;
+      case error.TIMEOUT:
+        alert("Tempo de espera para obter a localização do usuário esgotado.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("Ocorreu um erro desconhecido.");
+        break;
+    }
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(initMap, showError);
+  } else {
+    alert("Geolocalização não é suportada pelo seu navegador.");
+  }
