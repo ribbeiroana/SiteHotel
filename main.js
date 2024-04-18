62cbf65ef89e90654addfef12c67d361d33fbbef
ribbeiroana <anapribbeiro@gmail.com>
2024-04-17 22:00:01 -0300

    alterações nas imagens e cards

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function calcularTotal() {
-    // Obter os valores dos campos de entrada
+    // variaveis
     var dias = parseInt(document.getElementById("dias").value);
     var pessoas = parseInt(document.getElementById("pessoas").value);
 
@@ -12,3 +12,16 @@ function calcularTotal() {
   // Ativar o carrossel
   var myCarousel = document.querySelector('#carouselExample');
   var carousel = new bootstrap.Carousel(myCarousel);
+
+//carrosel automático 
+document.addEventListener("DOMContentLoaded", function() {
+  // Inicializa o primeiro carrossel
+  var carousel1 = new bootstrap.Carousel(document.getElementById('carouselExample1'), {
+      interval: 3000 
+  });
+
+  // Inicializa o segundo carrossel
+  var carousel2 = new bootstrap.Carousel(document.getElementById('carouselExample2'), {
+      interval: 3000 
+  });
+});
