9f57bd31dc47af41928c6c80b7c57c540ddaef14
Ana Paula <132491694+ribbeiroana@users.noreply.github.com>
2024-05-30 00:44:31 -0300

    atualização_2.0

--- SiteHotel/README.md
+++ SiteHotel/README.md
@@ -1,2 +1,47 @@
-Atualização da atividade desenvolvida em sala de aula 
-CSS adicionado no código
+# Site para Hotel 
+Esse é um projeto de um site para um hotel chamado "Diamond Hotel". Vamos analisá-lo:
+
+## Estrutura HTML:
+
+1. Header: Contém o logotipo e o nome do hotel.
+2. Nav: Lista de links de navegação.
+3. Sections: Dividem o conteúdo em seções como "Hospedagem", "Gastronomia", "Lazer", "Reserva", "Alguma Dúvida?" e "Localização".
+4. Footer: Contém o logotipo novamente e um link para a seção de localização.
+Conteúdo:
+5. Imagens: São utilizadas para ilustrar o hotel, suas instalações e quartos.
+6. Formulários: Permitem que os usuários inscrevam-se na newsletter e façam reservas, fornecendo informações como nome, CPF, e-mail, data de chegada, quantidade de dias e pessoas, tipo de quarto, entre outros.
+<div>
+    <img src="imgReadme/home.png" alt="">
+</div>
+
+## Funcionalidades JavaScript:
+1. Ativação de Carrosséis: Utiliza o Bootstrap Carousel para ativar os carrosséis de imagens.
+2. Máscara de CPF: Adiciona automaticamente pontos e traço à medida que o usuário digita o CPF.
+3. Validação de E-mail: Verifica se o e-mail inserido é válido.
+4. Cálculo de Diária: Calcula o valor total da diária com base na quantidade de pessoas, dias e tipo de quarto selecionados no formulário de reserva.
+<div>
+    <img src="imgReadme/gastronomia.png" alt="">
+</div>
+
+## Funcionalidades CSS e Bootstrap:
+1. Estilização do Cabeçalho e Menu de Navegação:
+2. Estilização dos Cartões:
+3. Estilização dos Formulários:
+4. Estilos Responsivos:
+5. Utilização de Componentes CSS do Bootstrap:
+
+<div>
+    <img src="imgReadme/reserva.png" alt="">
+</div>
+<br>
+
+## Tecnologias 
+<div style="display: inline_block">
+  <img align="center" alt="html5" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+  <img align="center" alt="css" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+  <img align="center" alt="js" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+  <img align="center" alt="bootstrap" src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" />
+  <img align="center" alt="git" src="https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white"/>
+
+</div>
+
--- SiteHotel/js/main.js
+++ SiteHotel/js/main.js
@@ -1,36 +1,52 @@
-function calcularTotal() {
-    // variaveis
-    var dias = parseInt(document.getElementById("dias").value);
-    var pessoas = parseInt(document.getElementById("pessoas").value);
-
-    var valorDiaria = 100; // Valor da diária por pessoa
-    var total = dias * pessoas * valorDiaria;
-
-    // Exibir o total calculado na página
-    document.getElementById("total").innerText = "Total da Hospedagem: R$" + total.toFixed(2);
-}
-  // Ativar o carrossel
-  var myCarousel = document.querySelector('#carouselExample');
-  var carousel = new bootstrap.Carousel(myCarousel);
+// Ativar o carrossel
+var myCarousel = document.querySelector('#carouselExample');
+var carousel = new bootstrap.Carousel(myCarousel);
 
 //carrosel automático 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Inicializa o primeiro carrossel
   var carousel1 = new bootstrap.Carousel(document.getElementById('carouselExample1'), {
-      interval: 3000 
+    interval: 3000
   });
 
   // Inicializa o segundo carrossel
   var carousel2 = new bootstrap.Carousel(document.getElementById('carouselExample2'), {
-      interval: 3000 
+    interval: 3000
   });
 });
-function calcularReserva() {
-  var quartoSelecionado = document.getElementById("quarto").value;
-  var quantidadePessoas = document.getElementById("pessoas").value;
-  var quantidadeDias = document.getElementById("dias").value;
 
-  var valorReserva = parseInt(quartoSelecionado) * parseInt(quantidadePessoas) * parseInt(quantidadeDias);
 
-  document.getElementById("resultado").innerText = "O valor total da reserva é: " + valorReserva + " reais.";
+function mascaraCPF(cpf) {
+  cpf.value = cpf.value
+    .replace(/\D/g, '') // Remove qualquer caractere não numérico
+    .replace(/(\d{3})(\d)/, '$1.$2') // Coloca o ponto após os primeiros 3 dígitos
+    .replace(/(\d{3})(\d)/, '$1.$2') // Coloca o ponto após os próximos 3 dígitos
+    .replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Coloca o traço antes dos últimos 2 dígitos
 }
+
+
+function validarEmail(email) {
+  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+var email = "example@email.com";
+if (validarEmail(email)) {
+  console.log("O e-mail é válido.");
+} else {
+  console.log("Por favor, insira um e-mail válido.");
+}
+
+function calcularDiaria(event) {
+  event.preventDefault(); // Evita que o formulário seja enviado
+
+  var numPessoas = parseInt(document.getElementById("pessoas").value);
+  var numDias = parseInt(document.getElementById("dias").value);
+  var tipoQuarto = parseInt(document.getElementById("quarto").value);
+
+  var valorDiaria = numPessoas * numDias * tipoQuarto;
+
+  document.getElementById("resultado").textContent = "O valor total da diária é R$ " + valorDiaria.toFixed(2);
+}
+
+var botao = document.getElementById("botaoReserva");
+botao.addEventListener("click", calcularDiaria);
